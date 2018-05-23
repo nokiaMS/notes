c67e577c56a9affb9f19a900f3a67a0a6b765a05
Guo Xu <guo.xu@wanglutech.com>
2018-05-22 18:50:45 -0700

    aaa

---
+++ perf_test/perf_shoot.js
@@ -0,0 +1,69 @@
+#!/usr/bin/node
+
+const Web3 = require('web3')
+const config = require('./config.js')
+
+if(typeof(process.argv[2]) == 'undefined') {
+	console.log("Usage: " + process.argv[1] + " <index>")
+	return
+} else {
+	var index = parseInt(process.argv[2])
+	console.log("The process index is: " + index)
+}
+
+var procInfo = config.procList[index]
+console.log(procInfo.url)
+
+//if object web3 has not been created, then we create it. if not, we should re-use the old one.
+if(typeof web3 !== 'undefined') {
+	web3 = new Web3(web3.currentProvider)
+} else {
+	web3 = new Web3(new Web3.providers.HttpProvider(procInfo.url))
+}
+
+if(web3.isConnected() !== true) {
+	console.log("Can not connect to web3 server.")
+	return
+}
+
+//Connected to web3 server.
+var eth = web3.eth
+var personal = web3.personal
+var totalTransactionCount = 0
+
+//unlock account.
+personal.unlockAccount(procInfo.fromAccount, procInfo.password, 60*60*100)  //100 hours
+
+//send transaction.
+function sendTransactionBatch() {
+	eth.sendTransaction({from:procInfo.fromAccount,to:procInfo.toAccount,value:"1"}, 
+    		function(error, result){
+    			if(error) {
+        			console.error(error)
+    			} else {
+				totalTransactionCount += 1
+        			console.log("Total Transaction count: " + totalTransactionCount)
+    			}
+		})
+}
+
+function runSmartContractFunc() {
+	var executeStr = "global.contractInstance." + config.testFunction + ".sendTransaction({from:'" + procInfo.fromAccount + "', gas: 2000000})"
+	console.log(executeStr)
+	var executeStrWithLog = "console.log(" + executeStr + ".toString())"
+	eval(executeStrWithLog)
+}
+
+var testType = procInfo.testType
+if(testType == 1) {
+	console.log("Running money transfer case...")
+	setInterval(sendTransactionBatch,procInfo.interval)
+}
+
+if(testType == 2) {
+	console.log("Running smart contract case...")
+	const hwContract = web3.eth.contract(JSON.parse(abi))
+	global.contractInstance = hwContract.at(procInfo.contractAddress)
+	setInterval(runSmartContractFunc,procInfo.interval)
+}
+
---
+++ perf_test/perf_tranPerMin.js
@@ -0,0 +1,49 @@
+#!/usr/bin/node
+
+const Web3 = require('web3')
+
+if(typeof(process.argv[2]) == 'undefined') {
+	console.log("Usage: " + process.argv[1] + " <url>")
+	return
+}
+
+var url = process.argv[2]
+console.log("Connecting to " + url)
+
+//if object web3 has not been created, then we create it. if not, we should re-use the old one.
+if(typeof web3 !== 'undefined') {
+	web3 = new Web3(web3.currentProvider)
+} else {
+	web3 = new Web3(new Web3.providers.HttpProvider(url))
+}
+
+if(web3.isConnected() !== true) {
+	console.log("Can not connect to web3 server.")
+	return
+}
+
+//Connected to web3 server.
+var eth = web3.eth
+var oldBlockNum = eth.blockNumber
+
+//Calculate the transaction count of per minutes.
+function calcTranCount(startBlockNum, endBlockNum) {
+	var totalTranCount = 0.0000
+	for(var i = startBlockNum + 1; i <= endBlockNum; i++) {
+		totalTranCount += eth.getBlockTransactionCount(i)
+		//console.log("Total transaction count: " + totalTranCount/min)
+	}
+	console.log("    Blocks:[from: " + startBlockNum + " to: " + endBlockNum + " tranCnt: " + totalTranCount + " ]")
+}
+
+//Show the result of one minutes.
+function getResult() {
+	var curBlockNum = eth.blockNumber
+	console.log("Blocks per minute: " + (curBlockNum - oldBlockNum))
+	calcTranCount(oldBlockNum, curBlockNum)
+	oldBlockNum = curBlockNum	
+}
+
+//Get the count of blocked per minute.
+setInterval(getResult,60*1000)
+
